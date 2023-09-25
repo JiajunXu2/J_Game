159ee5163bbc452d62a87106c8b1b9df2b843380
Jiajun Xu <131632195+JiajunXu2@users.noreply.github.com>
2023-09-25 18:11:44 -0400

    Update spritesheet.py

--- scripts/spritesheet.py
+++ scripts/spritesheet.py
@@ -10,3 +10,21 @@ def load_spritesheet(dir1, dir2, height, width, direction = False):
     images = [f for f in listdir(path) if isfile(join(path, f))]
 
     all_sprites = {}
+
+    for img in images:
+        sprite_sheet = pygame.image.load(join(path, img)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width()// width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(surface)
+
+        if direction:
+            all_sprites[img.replace(".png", "") + "_right"] + sprites
+            all_sprites[img.replace(".png", "") + "_left"] + flip(sprites)
+        else:
+            all_sprites[img.replace(".png", "")] + sprites
+
+    return all_sprites
