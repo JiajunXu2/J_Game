7a42b71239f420d0fb1eac026b4f46deeb740297
Jiajun Xu <jiajunxu2@gmail.com>
2023-09-27 22:16:36 -0400

    added the "update_sprite" function to Player Class

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ class Game:
 
   def run(self):
     while True:
-      # self.DISPLAYSURF.blit(self.img, self.img_pos)
-
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
--- scripts/player.py
+++ scripts/player.py
@@ -1,7 +1,11 @@
 import pygame
+from scripts.spritesheet import load_spritesheet
+
 class Player(pygame.sprite.Sprite):
   GRAVITY = 1
   COLOR = (255, 0, 0)
+  SPRITES = load_spritesheet("images", "pinkie", 32, 32, True)
+  ANIMATION_DELAY = 3
 
   def __init__(self, x, y, width, height) -> None:
     self.rect = pygame.Rect(x, y , width, height)
@@ -24,10 +28,18 @@ class Player(pygame.sprite.Sprite):
     self.y_velocity = velocity
     if self.direction != "right":
       self.direction = "right"
-  
+
+  def update_sprite(self):
+    spritesheet = "idle"
+    if self.x_velocity != 0:
+      spritesheet = "run"
+    spritesheet_name = spritesheet + "_" + self.direction
+    sprites = self.SPRITES[spritesheet_name]
+
   # called once every frame to control movement and update directions
   def loop(self, fps):
     self.move(self.x_velocity, self.y_velocity)
+    self.update_sprite()
 
   def draw(self, window):
     pygame.draw.rect(window, self.COLOR, self.rect)
